8a30f3f5562250d31c713cec7c7bc1f2c2529a47
MrDrElliot <133075253+MrDrElliot@users.noreply.github.com>
2026-03-13 13:23:57 -0500

    Asertion

--- Engine/Source/Runtime/Scripting/Lua/Traits.cpp
+++ Engine/Source/Runtime/Scripting/Lua/Traits.cpp
@@ -1,11 +1,13 @@
 ﻿#include "pch.h"
 #include "Traits.h"
+#include "Core/Assertions/Assert.h"
 
 namespace Lumina::Lua
 {
     uint16 FTypeIndex::Next()
     {
         static uint16_t GNextID = 1;
-        return GNextID++;
+        DEBUG_ASSERT(GNextID <= eastl::numeric_limits<uint16>::max(), "Hit the maximum number of allowed userdata!");
+        return static_cast<uint16>(GNextID++);
     }
 }
